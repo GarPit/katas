f84a250726fb132ef5bf3736c0c4196bf2c08ba8
Igor Petrov <garik.piton@gmail.com>
2016-11-13 23:41:17 +0300

    harry potter kata

--- lib/harry_potter_books.rb
+++ lib/harry_potter_books.rb
@@ -1,7 +1,28 @@
 class HarryPotterBooks
   def initialize(first, second, third, fourth, fifth)
+      @first = first
+      @second = second
+      @third = third
+      @fourth = fourth
+      @fifth = fifth
   end
 
-  def optimal_price
+  def optimal_price(*book_counts)
+      current_group = if book_counts.size > 0
+                          book_counts
+                      else
+                          [@first, @second, @third, @fourth, @fifth]
+                      end
+      group_of_different = current_group.reject {|number| number.zero? }
+      discounted_price = case group_of_different.size
+        when 5 then 0.75
+        when 4 then 0.8
+        when 3 then 0.9
+        when 2 then 0.95
+        else
+            return 8 * group_of_different[0].to_i
+        end
+      next_group = group_of_different.map {|number| number - 1}
+      return 8 * group_of_different.size * discounted_price + optimal_price(*next_group)
   end
 end
--- spec/harry_potter_books_spec.rb
+++ spec/harry_potter_books_spec.rb
@@ -10,5 +10,40 @@ describe HarryPotterBooks do
       end
 
     end
+    context 'when 2 different books in cart' do
+        subject { HarryPotterBooks.new(3,1,0,0,0) }
+
+        it "returns 5% discount on 2 books" do
+            expect(subject.optimal_price).to be_eql(31.2)
+        end
+    end
+    context 'when 3 different books in cart' do
+        subject { HarryPotterBooks.new(3,1,1,0,0) }
+
+        it "returns 10% discount on 3 books" do
+            expect(subject.optimal_price).to be_eql(37.6)
+        end
+    end
+    context 'when 4 different books in cart' do
+        subject { HarryPotterBooks.new(1,1,1,3,0) }
+
+        it "returns 20% discount on 4 books" do
+            expect(subject.optimal_price).to be_eql(41.6)
+        end
+    end
+    context 'when 5 different books in cart' do
+        subject { HarryPotterBooks.new(1,1,1,3,1) }
+
+        it "returns 25% discount on 5 books" do
+            expect(subject.optimal_price).to be_eql(46.0)
+        end
+    end
+    context 'when several groups of different books in cart' do
+        subject { HarryPotterBooks.new(2,2,2,1,1) }
+
+        it "returns optimal price for such cart" do
+            expect(subject.optimal_price).to be_eql(51.6)
+        end
+    end
   end
 end
